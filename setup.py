9b9905d5fd6acd28e6ab038ef094cdcf15a71266
Philipp Ernst <phil23940@yahoo.de>
2020-09-29 12:07:34 +0200

    version 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pytorch_radon',
-    version='0.1.3',
+    version='0.1.4',
     author='Philipp Ernst',
     author_email='phil23940@yahoo.de',
     packages=['pytorch_radon'],
